5cb16cc8c1a516d12b463238e002a8bbe66f2025
Suhail Khan <26044674+suhailskhan@users.noreply.github.com>
2022-04-11 13:39:07 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
-python -m venv venv
+python3 -m venv venv
 ./venv/bin/pip install --upgrade pip
 ./venv/bin/pip install -r requirements.txt
+
+echo \n
+echo "------"
+echo "venv created."
+echo "Use '. venv/bin/activate' to use the venv."
